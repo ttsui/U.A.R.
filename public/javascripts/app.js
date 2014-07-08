518ad07518a532da72ece3b6906c8ebccc64a9f8
Tony Tsui <tsui.tony@gmail.com>
2014-07-08 09:13:25 +0100

    Render multiple user interactions.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,26 +1,43 @@
-var UserEvents = Backbone.Collection.extend({
-    url: 'api/userevents'
-});
-
 var UserAction = Backbone.Model.extend({});
 
+var UserActions = Backbone.Collection.extend({
+    url: 'api/users/actions',
+    model: UserAction
+});
+
 var ActionView = Backbone.Marionette.ItemView.extend({
-    template: Handlebars.compile($('#idea-link-clicked-template'))
+    template: Handlebars.compile($('#idea-link-clicked-template').html())
+//    template: _.template('<%-author_name%>')
+});
+
+var ActionStack = Backbone.Marionette.CollectionView.extend({
+    childView: ActionView
 });
 
 (function() {
-    var aUserAction = new UserAction({
-        "id": "68E27356-0064-11E4-AEFD-96F9DE56FB62",
-        "author_name": "Marina Mata",
-        "eventType": "IdeaLinkClicked",
-        "recipient": "fcruz",
-        "stock_name": "Lojas Renner SA",
-        "stock_ticker": "LREN3 BZ",
-        "timestamp": "2014-06-30T14:39:57.128+0000",
-        "message": "IdeaLinkClicked"
-    });
+    var actions = new UserActions([
+        {
+            "id": "68E27356-0064-11E4-AEFD-96F9DE56FB62",
+            "author_name": "Marina Mata",
+            "eventType": "IdeaLinkClicked",
+            "recipient": "fcruz",
+            "stock_name": "Lojas Renner SA",
+            "stock_ticker": "LREN3 BZ",
+            "timestamp": "2014-06-30T14:39:57.128+0000",
+            "message": "IdeaLinkClicked"
+        }, {
+            "id": "AAA27356-0064-11E4-AEFD-96F9DE56FB62",
+            "author_name": "John Smite",
+            "eventType": "IdeaLinkClicked",
+            "recipient": "fcruz",
+            "stock_name": "Startbucks Inc",
+            "stock_ticker": "SBU L",
+            "timestamp": "2014-06-03T04:39:57.128+0000",
+            "message": "IdeaLinkClicked"
+        }
+    ]);
 
-    var view = new ActionView({model: aUserAction});
+    var view = new ActionStack({ collection: actions });
 
     var mainRegion = new Backbone.Marionette.Region({
         el: '#main-container'
